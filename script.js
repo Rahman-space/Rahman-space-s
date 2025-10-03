9ddae975a8bf641d5af1aa12170ec4eebd0ef95b
Rahman Illiasu <rahmanilliasu00@gmail.com>
2025-10-03 02:26:03 +0000

    Updated homepage and styles

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-<script>
-  document.addEventListener("DOMContentLoaded", () => {
-    const menuToggle = document.querySelector(".menu-toggle");
-    const navLinks = document.querySelector(".nav-links");
-    const links = document.querySelectorAll(".nav-links a");
+  
+  <script>
+    document.addEventListener("DOMContentLoaded", () => {
+      const menuToggle = document.querySelector(".menu-toggle");
+      const navLinks = document.querySelector(".nav-links");
+      const links = document.querySelectorAll(".nav-links a");
 
-    // Toggle sidebar
-    menuToggle.addEventListener("click", () => {
-      navLinks.classList.toggle("show");
-    });
+      menuToggle.addEventListener("click", () => {
+        navLinks.classList.toggle("show");
+      });
 
-    // Close sidebar when a link is clicked
-    links.forEach(link => {
-      link.addEventListener("click", () => {
-        navLinks.classList.remove("show");
+      links.forEach(link => {
+        link.addEventListener("click", () => {
+          navLinks.classList.remove("show");
+        });
       });
     });
-  });
-</script>
+  </script>
+
+
